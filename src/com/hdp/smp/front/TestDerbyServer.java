6bd5575672229f99e55e96a01b77796cdaac6703
Tony-Huang <hdp214@163.com>
2015-04-14 19:20:30 +0800

    code for derby network server
    code for derby network server

---
+++ src/com/hdp/smp/front/TestDerbyServer.java
@@ -0,0 +1,13 @@
+package com.hdp.smp.front;
+
+public class TestDerbyServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
