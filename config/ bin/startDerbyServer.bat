b6e72cbcf7a7935ed208fe24f0449dbce81f6b92
Tony-Huang <hdp214@163.com>
2017-11-29 17:15:46 +0800

    Update startDerbyServer.bat

--- config/ bin/startDerbyServer.bat
+++ config/ bin/startDerbyServer.bat
@@ -19,7 +19,8 @@
 @cd ..
 @set CONFIG_BASE=%CD%
 @set DERBY_LIB_DIR=%CONFIG_BASE%\sql\derby\lib
-@set CLASSPATH=%DERBY_LIB_DIR%\*
+@set CLASSPATH=.;%DERBY_LIB_DIR%\*
+@set PATH=%JAVA_HOME%\bin;%PATH%
 
 @cd "%CONFIG_BASE%"
 @java -Xms256m -Xmx256m -server -cp "%CLASSPATH%" org.apache.derby.drda.NetworkServerControl start -noSecurityManager
