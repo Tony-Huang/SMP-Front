9b15825af1811389216f71ec1e13161663f059ac
Tony-Huang <hdp214@163.com>
2017-11-29 17:22:44 +0800

    Update createSchema.sh

--- config/sql/derby/createSchema.sh
+++ config/sql/derby/createSchema.sh
@@ -4,7 +4,7 @@ echo ------creating schema of DB------
 
 
 CLASSPATH=lib/derby.jar:lib/derbyclient.jar:lib/derbytools.jar:lib/derbyrun.jar:$CLASSPATH
-JAVA_HOME=/opt/IBM/WLP85/java/java_1.7.1_64
+#JAVA_HOME=/opt/IBM/WLP85/java/java_1.7.1_64
 PATH=$JAVA_HOME/bin:$PATH
 echo Create  derby database...
 java -Xmx128m -Xms128m -jar lib/derbyrun.jar ij -p smpdb.properties schema.sql
